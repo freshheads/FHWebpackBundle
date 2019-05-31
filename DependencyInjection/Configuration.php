8b5c324a0693aa35a17b78d25cb30a42c9a54a67
Evert Harmeling <evert.harmeling@freshheads.com>
2019-05-31 16:36:03 +0200

    Fixes TreeBuilder deprecation warning

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -14,15 +14,26 @@ namespace FH\Bundle\WebpackBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
+use function method_exists;
+
 /**
  * @author Joris van de Sande <joris.van.de.sande@freshheads.com>
  */
 class Configuration implements ConfigurationInterface
 {
+    private const ROOT_NAME = 'fh_webpack';
+    
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('fh_webpack');
+        $treeBuilder = new TreeBuilder(self::ROOT_NAME);
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root(self::ROOT_NAME);
+        }
+        
         $rootNode
             ->children()
                 ->scalarNode('stats_filename')
